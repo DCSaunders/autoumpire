80eb9aa1331cbb058c499c1c5803cd280aee9ae4
DCSaunders <ds636@cam.ac.uk>
2016-10-23 23:28:42 +0100

    working mailer

--- mailer.py
+++ mailer.py
@@ -3,14 +3,22 @@
 import smtplib
 from email.mime.text import MIMEText
 
-def send_targets(address):
-    sender = 'assassins-umpire@srcf.ucam.org'
-    msg = MIMEText('meep')
-    msg['Subject'] = '[Assassins] Targets'
-    msg['From'] = sender
-    msg['To'] = address
-    s = smtplib.SMTP()
-    s.sendmail(sender, address, msg.as_string())
-    s.quit()
+class Mailer(object):
+    def __init__(self, crsid, pwd, host):
+        self.crsid = crsid
+        self.pwd = pwd
+        self.host = host
+        self.sender = 'assassins-umpire@srcf.ucam.org'
+    
+    def send_message(self, address):
+        msg = MIMEText('meep')
+        msg['Subject'] = '[Assassins] Targets'
+        msg['From'] = self.sender
+        msg['To'] = address
+        s = smtplib.SMTP(self.host, port=587)
+        s.starttls()
+        s.login(self.crsid, self.pwd)
+        s.sendmail(self.sender, address, msg.as_string())
+        s.quit()
 
-send_targets('address')
+    send_message('fake@address.org')
