b3ae0611259f16e9b0b4c7e8519e048922bd42aa
DCSaunders <ds636@cam.ac.uk>
2016-10-30 18:23:50 +0000

    improved user/pwd reading for mailer

--- mailer.py
+++ mailer.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
-
+import getpass
 import smtplib
 from email.mime.text import MIMEText
 
 class Mailer(object):
-    def __init__(self, crsid, pwd, host):
-        self.crsid = crsid
-        self.pwd = pwd
+    def __init__(self, host):
         self.host = host
         self.sender = 'assassins-umpire@srcf.ucam.org'
-    
+
     def send_message(self, address):
         msg = MIMEText('meep')
         msg['Subject'] = '[Assassins] Targets'
@@ -17,8 +15,12 @@ class Mailer(object):
         msg['To'] = address
         s = smtplib.SMTP(self.host, port=587)
         s.starttls()
-        s.login(self.crsid, self.pwd)
+        crsid = raw_input('Enter CRSID (excluding @cam section): ')
+        pwd = getpass.getpass('Enter Hermes password for {} (password will not be visible when typed): '.format(crsid))
+        s.login(crsid, pwd)
         s.sendmail(self.sender, address, msg.as_string())
         s.quit()
 
-    send_message('fake@address.org')
+
+
+    
