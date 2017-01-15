0f27024685994a5785a861334217d39f3a7ecd41
DCSaunders <ds636@cam.ac.uk>
2017-01-15 23:53:02 +0000

    setup for initialising

--- game_runner.py
+++ game_runner.py
@@ -2,19 +2,23 @@
 import utils
 import constants
 from game_reader import Lexer, Interpreter
-
+from player import PolicePlayer
+from targeter import Graph
 
 class GameRunner(object):
-    def __init__(self, game_file, start_date, player_dict, reporter):
+    def __init__(self, game_file, start_date, player_dict):
         self.date = start_date
         self.game_file = game_file
-        self.players = []
-        self.name_set = set(player_dict.keys())
-        with open(game_file, 'r') as f:
+        self.player_dict = player_dict
+        self.event_players = []
+        self.name_set = set(self.player_dict.keys())
+
+    def run_game(self, reporter):
+        with open(self.game_file, 'r') as f:
             for line in f:
-                self.run_game(line, player_dict, reporter)
+                self.run_event(line, reporter)
     
-    def run_game(self, line, player_dict, reporter):
+    def run_event(self, line, reporter):
         lexer = Lexer(line, self.name_set)
         interpreter = Interpreter(lexer)
         interpreter.event()
@@ -26,19 +30,18 @@ class GameRunner(object):
             reporter.new_date(formatted_date)
         summaries, event_players = [], set()
         for token in events:
-            self.players = [player_dict[name] for name in events[token]]
-            event_players = event_players.union(self.players)
+            self.event_players = [self.player_dict[name]
+                                  for name in events[token]]
+            event_players = event_players.union(self.event_players)
             self.get_token_summary(token, summaries, event_time)
         if event_time:
             reporter.new_report(summaries, event_players, event_time)
             
     def get_token_summary(self, token, summaries, event_time):
         summary_pseuds = ', '.join(
-            [player.pseudonym for player in self.players])
+            [player.pseudonym for player in self.event_players])
         if token.type == constants.KILLS:
             summaries.append(self.kill_event(event_time))
-        elif token.type == constants.BONUS:
-            summaries.append(self.bonus_event(token.value, summary_pseuds))
         elif token.type == constants.PLAYER_OP:
             method_name = token.value.lower() + '_str'
             summary_method = getattr(self, method_name)
@@ -48,43 +51,73 @@ class GameRunner(object):
         return "An event happens involving {}.".format(summary_pseuds)
 
     def remove_str(self, summary_pseuds):
-        for player in self.players:
+        for player in self.event_players:
             player.remove_from_game()
         return "{} removed from the game.".format(summary_pseuds)
 
     def casual_str(self, summary_pseuds):
-        for player in self.players:
+        for player in self.event_players:
             player.make_casual()
         return "{} now playing casually.".format(summary_pseuds)
 
+    def kill_event(self, kill_time):
+        killer = self.event_players[0]
+        dead_players = self.event_players[1:]
+        kill_time = utils.get_datetime(self.date, event_time=kill_time)
+        killer.killed(dead_players, kill_time)
+        dead_list = [player.represent(kill_time)
+                     for player in dead_players]
+        dead_str = ' and '.join(dead_list)
+        return '{} kills {}.'.format(killer.represent(kill_time),
+                                     dead_str)
+
+class ShortGameRunner(GameRunner):
+    def __init__(self, game_file, start_date, player_dict):
+        super(ShortGameRunner, self).__init__(
+            game_file, start_date, player_dict)
+        
+    # Score the playerlist
+    def score(self):
+        for player in self.player_dict.values():
+            player.calc_points()
+
+    def get_token_summary(self, token, summaries, event_time):
+        if token.type == constants.BONUS:
+            summary_pseuds = ', '.join(
+                [player.pseudonym for player in self.event_players])
+            summaries.append(
+                self.bonus_event(token.value, summary_pseuds))
+        else:
+            super(ShortGameRunner, self).get_token_summary(
+                token, summaries, event_time)
+            
+    def run_game(self, reporter):
+        with open(self.game_file, 'r') as f:
+            for line in f:
+                self.run_event(line, reporter)
+        self.score()
+
     def bonus_event(self, points, summary_pseuds):
-        for player in self.players:
+        for player in self.event_players:
             player.bonus(points)
         return '{} bonus points to {}.'.format(points, summary_pseuds)
 
-    def kill_event(self, kill_time):
-        killer = self.players[0]
-        dead_players = self.players[1:]
-        kill_time = utils.get_datetime(self.date, event_time=kill_time)
-        killer.killed(dead_players, kill_time)
-        dead_list = [player.represent(kill_time) for player in dead_players]
-        dead_str = ' and '.join(dead_list)
-        return '{} kills {}.'.format(killer.represent(kill_time), dead_str)
-
-class ShortGameRunner(GameRunner):
-    def __init__(self, game_file, start_date, player_dict, reporter):
-        super(ShortGameRunner, self).__init__(game_file, start_date, player_dict, reporter)
-        
-
 
 class LongGameRunner(GameRunner):
-    def __init__(self, game_file, start_date, player_dict, reporter):
-        self.police = {}
-        self.get_police(player_dict)
+    def __init__(self, game_file, start_date, player_dict):
         super(LongGameRunner, self).__init__(game_file, start_date,
-            player_dict, reporter)
+            player_dict)
+        self.police = {}
+        self.get_police()
+        self.targeter = Graph(len(self.player_dict) - len(self.police))
+        self.targeter.initialise(self.player_dict.values())
+        
+    def get_police(self):
+        for name, player in self.player_dict.items():
+            if type(player) == PolicePlayer:
+                self.police[name] = player
 
-    def get_police(self, player_dict):
-        for name, player in player_dict.items():
-            print name, type(player)
-  
+    def run_game(self, reporter):
+        with open(self.game_file, 'r') as f:
+            for line in f:
+                self.run_event(line, reporter)
--- targeter.py
+++ targeter.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 import numpy as np
+from player import PolicePlayer
 
 class TestPlayer(object):
+    # Dummy class for testing
     def __init__(self, name):
         self.name = name
-        self.targets = []
-        self.assassins = []
+        self.targets = set()
+        self.assassins = set()
 
 class Node(object):
     def __init__(self, name=None):
@@ -25,30 +27,12 @@ class Node(object):
                 return False
         return True
             
-
 class Graph(object):
     def __init__(self, node_count):
         self.nodes = []
         self.node_count = node_count
         for i in range(0, self.node_count):
             self.nodes.append(Node(i))
-
-    def get_next_prime(self):
-        if (self.node_count + 1) % 2 == 0:
-            n = self.node_count + 2
-        else:
-            n = self.node_count + 1
-        while(not self.is_prime(n)):
-            n += 2
-        return n
-
-    def is_prime(self, n):
-        test = 3
-        while test <= np.sqrt(n):
-            if n % test == 0:
-                return False
-            test += 2
-        return True
 
     def construct(self):
         for i, node in enumerate(self.nodes):
@@ -72,7 +56,22 @@ class Graph(object):
                 for t in node.targets:
                     f.write('"{}" -> "{}";\n'.format(node.name, t.name))
             f.write('}\n')
-            
+
+    def initialise(self, player_list):
+        self.construct()
+        to_assign = [player for player in player_list
+                     if type(player) is not PolicePlayer]
+        self.seed(to_assign)
+     
+        
+    def seed(self, to_assign):
+        # Seeds in e.g. nodes 0, 9, 18, 27...
+        num_seeds = int(self.node_count / 9) + 1
+        to_seed = [player for player in to_assign if player.seed]
+        
+        to_seed = sorted(to_seed, key=lambda x: x.seed)[:num_seeds]
+        print 'Seeding {}'.format(', '.join([p.name for p in to_seed]))
+        
 np.random.seed(1234)
 player_list = []
 player_count = 28
@@ -84,21 +83,14 @@ targets.print_graph()
 targets.dotfile_graph()
 
 '''
-Need to store targets in an adjacency list like so:
-targets = [v0 v1 v2 v3 v4 v5]
-targets[0] = v0 = [1 2 5] - player 0 has targets 1, 2, 5
-Need to avoid self-cycles, multiple edges (could be sets rather than lists), and 3-cycles (target triangles where each vertex is the target of the other two.) 
+Need to avoid self-cycles, multiple edges (could be sets rather than lists), and 3-cycles (target triangles where each vertex is the target of the other two.) In other words: 
 
 - I cannot target myself.
 - I cannot target my assassin
-- I cannot target my target's target
+- I would like to avoid targetting the assassin/target of my assassin/target
 
-A player must always have three distinct
-targets and three distinct assassins, and a player may not target their own
-assassins (or indeed themselves). In graph theoretic terms this is equivalent
-to saying that the targetting graph must remain `simple'
-
-For initial targetting, a completely random (simple) targetting graph is 
+Previous approach:
+"For initial targetting, a completely random (simple) targetting graph is 
 generated. While this meets the first two goals, it is very badly suited for 
 the third, on occasions blocking after fewer than 10 deaths. The reason for 
 this is the existence of `3-cycles', groups of 3 players, all of whom are 
@@ -114,6 +106,5 @@ sorting where these are equal). They are then placed into the targetting graph
 from the highest seed down, in such a way as to maximise the distance between 
 the player being added and all previous players. Obviously, this ceases to be 
 useful once we reach the point where all empty graph locations are adjacent to 
-an existing player. 
-Seeds in e.g. nodes 0, 9, 18, 27...
+an existing player."
 '''
